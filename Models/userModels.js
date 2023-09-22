d64939b367c107c7c4ddde0d4acdb11070a838bf
vikashkr-byte <vikash0719in1019@gmail.com>
2023-09-22 18:44:16 +0530

    updated user model to student and changed cpassword to confirm_password in model

--- Models/userModels.js
+++ Models/userModels.js
@@ -1,17 +1,17 @@
-const mongoose=require("mongoose")
-const userSchema=mongoose.Schema({
-    firstname:{type:String,required:true},
-    lastname:{type:String,required:true},
-    email:{type:String,required:true},
-    password:{type:String,required:true},
-    cpassword:{type:String,required:false},
-    mobile:{type:Number,required:true},
-    pincode:{type:Number,required:true}
-},{
-    versionKey:false
+const mongoose = require("mongoose")
+const userSchema = mongoose.Schema({
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    confirm_password: { type: String, required: false },
+    mobile: { type: Number, required: true },
+    pincode: { type: Number, required: true }
+}, {
+    versionKey: false
 })
 
-const UserModel=mongoose.model("student",userSchema)
-module.exports={
+const UserModel = mongoose.model("student", userSchema)
+module.exports = {
     UserModel
 }
